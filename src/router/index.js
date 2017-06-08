1648af79031ab0f3c34495b330682166ef01d9dd
wen <helloworld.wen@gmail.com>
2017-06-08 17:39:08 +0800

    修改侧边栏的样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Navigation from '@/components/Navigation'
 import Announcement from '@/components/Announcement'
 import Publish from '@/components/Publish'
 import CreateMeeting from '../components/meeting/Create'
 import Meeting from '../components/meeting/Meeting.vue'
 import Room from '../components/room/Room.vue'
 import Society from '../components/society/Society.vue'
+import Main from '../components/main/Main.vue'
 
 Vue.use(Router)
 
@@ -14,8 +14,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Navigation',
-      component: Navigation
+      name: 'main',
+      component: Main
     },
     {
       path: '/Announcement',
